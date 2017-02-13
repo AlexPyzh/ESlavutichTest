ceaafe9ee0d5bc944696ebbd8b82fb0006d1522d
AlexPyzh <alex.pyzh@gmail.com>
2017-02-13 10:46:28 +0200

    Добавьте файлы проекта.

---
+++ ESlavutichTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ESlavutichTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
